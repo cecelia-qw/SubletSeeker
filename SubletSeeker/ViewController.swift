9c801eb87727d9c8953077d483860b17c83f0830
Qingwan Cheng <chengqf@bc.edu>
2022-04-26 23:08:50 -0400

    Initial Commit

---
+++ SubletSeeker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubletSeeker
+//
+//  Created by Qingwan Cheng on 4/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
